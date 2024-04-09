8d736efbdcff7519685383ba15e30c9679d89a1c
n.kulakov <n.kulakov@ipc.kg>
2024-04-09 14:41:58 +0600

    added db

--- cmd/skill/main.go
+++ cmd/skill/main.go
@@ -2,10 +2,13 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"strings"
 
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/mrkovshik/yandex_practicum_go_adv/internal/logger"
+	"github.com/mrkovshik/yandex_practicum_go_adv/internal/store/pg"
 	"go.uber.org/zap"
 )
 
@@ -64,8 +67,14 @@ func run() error {
 		return err
 	}
 
-	// создаём экземпляр приложения, пока без внешней зависимости хранилища сообщений
-	appInstance := newApp(nil)
+	// создаём соединение с СУБД PostgreSQL с помощью аргумента командной строки
+	conn, err := sql.Open("pgx", flagDatabaseURI)
+	if err != nil {
+		return err
+	}
+
+	// создаём экземпляр приложения, передавая реализацию хранилища pg в качестве внешней зависимости
+	appInstance := newApp(pg.NewStore(conn))
 
 	logger.Log.Info("Running server", zap.String("address", flagRunAddr))
 	// обернём хендлер webhook в middleware с логированием и поддержкой gzip
